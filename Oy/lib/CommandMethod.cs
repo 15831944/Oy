97beab6a0900656cabbf9eb9285477ab0a7b679c
HXQ <supers.bluesky@gmail.com>
2019-08-19 16:42:39 +0800

    无

--- Oy/lib/CommandMethod.cs
+++ Oy/lib/CommandMethod.cs
@@ -10,7 +10,13 @@ namespace Oy.CAD2006.CommandMethod
     public class CommandMethod
     {
         [CommandMethod("TT")]
-        public void OpenMainForm() => Application.ShowModalDialog(new GUI.MainForm());
+        public void OpenMainForm()
+        {
+            Application.ShowModalDialog(new GUI.MainForm());
+            //GUI.MainForm mainForm = new GUI.MainForm();
+            //mainForm.ShowDialog(Application.MainWindow);
+            //mainForm.Dispose();
+        }
 
         [CommandMethod("Greating")]
         public void Greating() => lib.AutoCAD.Greating();
