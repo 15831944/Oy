a209cd38433dbf4d75e604422bcdadd3a36b03c9
HXQ <supers.bluesky@gmail.com>
2019-08-29 16:26:34 +0800

    无

--- Oy/lib/MyCommand.cs
+++ Oy/lib/MyCommand.cs
@@ -16,7 +16,7 @@ namespace Oy.CAD2006.CommandMethod
         //mainForm.ShowDialog(Application.MainWindow);
         //mainForm.Dispose();
         [CommandMethod("TT")]
-        public void OpenMainForm() => Application.ShowModalDialog( Application.MainWindow, new GUI.MainForm());
+        public void OpenMainForm() => Application.ShowModalDialog(Application.MainWindow, new GUI.MainForm());
 
 
         [CommandMethod("Greating")]
@@ -40,11 +40,11 @@ namespace Oy.CAD2006.CommandMethod
         [CommandMethod("test")]
         public void Test()
         {
-            ObjectId[] objectId =Interaction.GetSelection("\n选择多段线", "LWPOLYLINE");
+            ObjectId[] objectId = Interaction.GetSelection("\n选择多段线", "LWPOLYLINE");
             var ents = objectId.QSelect(x => x).ToList();
             List<TableData> tableDataList = new List<TableData>();
 
-            int StartBoundaryPointID=1;
+            int StartBoundaryPointID = 1;
 
             for (int i = 0; i < ents.Count; i++)
             {
@@ -52,7 +52,7 @@ namespace Oy.CAD2006.CommandMethod
                 var point3Ds = polyline.GetPolyPoints().ToArray();
                 //保留面积两位小数
                 double Area = System.Math.Round(polyline.Area, 2);
-                TableData tableData = new TableData(point3Ds, Area,i+1, 1, StartBoundaryPointID, (i + 1).ToString());
+                TableData tableData = new TableData(point3Ds, Area, i + 1, 1, StartBoundaryPointID, (i + 1).ToString());
                 StartBoundaryPointID += point3Ds.Length;
                 tableDataList.Add(tableData);
             }
@@ -69,16 +69,55 @@ namespace Oy.CAD2006.CommandMethod
 
 
 
-        
+
         [CommandMethod("test2")]
         public void Test2()
         {
             ObjectId[] ids = Interaction.GetSelection("\n选择多段线", "LWPOLYLINE");
-            double epsilon = Interaction.GetValue("\n最小距离",0.05);//过滤距离,默认大小需要设置文件
+            double epsilon = Interaction.GetValue("\n最小距离", 0.05);//过滤距离,默认大小需要设置文件
             ids.QForEach<Polyline>(poly =>
             {
                 int count = Algorithms.PolyClean_ReducePoints(poly, epsilon);
             });
+        }
+
+
+
+
+
+
+
+
+        [CommandMethod("test3")]
+        public static void Test3()
+        {
+            double value = Interaction.GetValue("\n曲线分割数量, 默认0为智能取点", 0);
+            if (double.IsNaN(value))
+            {
+                return;
+            }
+            int n = (int)value;
+
+            ObjectId[] ids = Interaction.GetSelection("\n选择多段线", "LWPOLYLINE");
+            var entsToAdd = new List<Polyline>();
+            ids.QForEach<Polyline>(poly =>
+            {
+                var pts = poly.GetPolylineFitPoints(n);
+                var poly1 = NoDraw.Pline(pts);
+                poly1.Layer = poly.Layer;
+                try
+                {
+                    poly1.ConstantWidth = poly.ConstantWidth;
+                }
+                catch
+                {
+                }
+                poly1.XData = poly.XData;
+                poly.Erase();
+                entsToAdd.Add(poly1);
+            });
+            entsToAdd.ToArray().AddToCurrentSpace();
+            Interaction.WriteLine("{0} handled.", entsToAdd.Count);
         }
     }
 }
